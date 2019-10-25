aa5a375744af83382d86cc891c3bd0ed1b96a278
By-Fabian <yuri.vilela@utec.edu.pe>
2019-10-25 15:32:46 -0500

    Creation of Proyect

---
+++ cmake-build-debug/CMakeFiles/Area51.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Area51"
+  "Area51.pdb"
+  "CMakeFiles/Area51.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Area51.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
