5792550d7631304353434c1d4ec26c5643f1e842
GaVizcacha <56094902+GaVizcacha@users.noreply.github.com>
2019-10-30 12:25:24 -0500

    Actualización jp

--- cmake-build-debug/CMakeFiles/Area51.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Area51.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/utec/Proyectos/Area51/main.cpp" "/home/utec/Proyectos/Area51/cmake-build-debug/CMakeFiles/Area51.dir/main.cpp.o"
+  "C:/Users/Jean Pierre/CLionProjects/Area51/main.cpp" "C:/Users/Jean Pierre/CLionProjects/Area51/cmake-build-debug/CMakeFiles/Area51.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
